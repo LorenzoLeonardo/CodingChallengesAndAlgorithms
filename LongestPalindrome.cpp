b5aad1cbee8446435ed6b9af27687313bf0fe703
Lorenzo Leonardo <lorenzo.leonardo.img@hotmail.com>
2022-01-28 14:23:20 +0800

    changes to longest palindrome

--- LongestPalindrome.cpp
+++ LongestPalindrome.cpp
@@ -1,29 +1,51 @@
 #include "Header.h"
 
-int expandAroundCenter(string s, int left, int right) 
-{
-    int L = left, R = right;
-    while ((L >= 0) && (R < s.length()) && (s[L] == s[R])) {
-        L--;
-        R++;
-    }
-    return R - L - 1;
-}
-
 string longestPalindrome(string s)
 {
-    if (s.empty() || s.length() < 1)
-        return "";
-    int start = 0, end = 0;
-    for (int i = 0; i < s.length(); i++)
+    long maxLength = 1;
+    long start = 0;
+    long len = s.length();
+    long low, high;
+
+    for (long i = 1; i < len; ++i)
     {
-        int len1 = expandAroundCenter(s, i, i);
-        int len2 = expandAroundCenter(s, i, i + 1);
-        int len = std::max(len1, len2);
-        if (len > end - start) {
-            start = i - (len - 1) / 2;
-            end = i + len / 2;
+        low = i - 1;
+        high = i;
+        while (low >= 0 && high < len
+            && s[low] == s[high])
+        {
+            --low;
+            ++high;
+        }
+
+        ++low; --high;
+        if (s[low] == s[high] && high - low + 1 > maxLength)
+        {
+            start = low;
+            maxLength = high - low + 1;
+        }
+
+        low = i - 1;
+        high = i + 1;
+        while (low >= 0 && high < len
+            && s[low] == s[high])
+        {
+            --low;
+            ++high;
+        }
+
+        ++low; --high;
+        if (s[low] == s[high] && high - low + 1 > maxLength)
+        {
+            start = low;
+            maxLength = high - low + 1;
         }
     }
-    return s.substr(start, end + 1);
+
+    size_t ans = maxLength;
+    string result;
+    while (ans--)
+        result.push_back(s[start++]);
+    
+    return result;
 }
--- main.cpp
+++ main.cpp
@@ -3,48 +3,5 @@
 
 int main()
 {
-   // string s = "{(([])[])[]]}";
-    //string n = "999999999999999999999";
-   // int k = 100000;
-    //printf("%d", superDigit(n, k));
-
-   // cout<<isBalanced(s);
-
-    MyQueue myqueue;
-    int t;
-    string input;
-
-    cin >> t;
-
-    string value;
-    string operation;
-
-    for (int i = 0; i <=t; i++)
-    {
-        std::getline(std::cin, input);
-        std::stringstream ss(input);
-
-        while (!ss.eof()) 
-        {
-            ss >> operation;
-            ss >> value;
-        }
-
-        if (operation == "1")
-        {
-            myqueue.enQueue(stol(value));
-        }
-        else if(operation == "2")
-        {
-            myqueue.deQueue();
-        }
-        else if (operation == "3")
-        {
-            myqueue.frontElement();
-        }
-
-        operation = "";
-        value = "";
-    }
     return 0;
 }
